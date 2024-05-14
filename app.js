dff5c70a5ce93c01a3bed134359d04b92419d074
elsanto0602 <santiagocantor16@gmail.com>
2024-05-13 19:58:55 -0500

    agrega cors para permitir peticion http

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+var cors = require('cors')
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -20,6 +22,8 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors())
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
